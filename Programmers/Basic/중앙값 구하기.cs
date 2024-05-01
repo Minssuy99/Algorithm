92a60c1123924543a64530c2096c434d94d55372
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-01 11:00:02 +0900

    Basic Programmers

---
+++ Programmers/Basic/중앙값 구하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array)
+    {
+        if(array.Length % 2 != 0) 
+        {
+            Array.Sort(array);
+        }
+        int answer = array[array.Length/2];
+        return answer;
+    }
+}
