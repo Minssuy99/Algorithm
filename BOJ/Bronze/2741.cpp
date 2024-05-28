01c4afab6a224f6882e3dc21e125dc1ca9b96863
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-29 03:44:25 +0900

    N 찍기

---
+++ BOJ/Bronze/2741.cpp
@@ -0,0 +1,34 @@
+// 2741. N 찍기
+// https://www.acmicpc.net/problem/2741
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; ++i)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+
+/* 
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        cout << i << endl;
+    }
+}
+ */
