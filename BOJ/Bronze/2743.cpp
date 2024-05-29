066315907f1ee3970179dfc8386b76cd4dc1a687
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-29 19:06:56 +0900

    단어 길이 재기

---
+++ BOJ/Bronze/2743.cpp
@@ -0,0 +1,17 @@
+// 2743. 단어 길이 재기
+// https://www.acmicpc.net/problem/2743
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string a;
+
+    cin >> a;
+
+    cout << a.length() << endl;
+
+    return 0;
+}
