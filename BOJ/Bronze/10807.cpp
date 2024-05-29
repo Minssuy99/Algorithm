1ed374dfe04558ab53def30ad3aac31051123c18
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-29 12:23:04 +0900

    개수 세기

---
+++ BOJ/Bronze/10807.cpp
@@ -0,0 +1,34 @@
+// 10807. 개수 세기
+// https://www.acmicpc.net/problem/10807
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, V, A = 0;
+
+    cin >> N;
+
+    int *arr = new int[N];
+
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    cin >> V;
+
+    for (int i = 0; i < N; ++i)
+    {
+        if (arr[i] == V)
+        {
+            A++;
+        }
+    }
+
+    cout << A << endl;
+
+    return 0;
+}
