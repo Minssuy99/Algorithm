42910ce38ae5397d43ab371715c4c83a0dbd3f72
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-29 21:41:47 +0900

    검증수

---
+++ BOJ/Bronze/2475.cpp
@@ -0,0 +1,30 @@
+// 2475. 검증수
+// https://www.acmicpc.net/problem/2475
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int result = 0;
+
+    int arr[5] = {};
+
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < arr_size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < arr_size; i++)
+    {
+        result += pow(arr[i], 2);
+    }
+
+    cout << result % 10 << endl;
+
+    return 0;
+}
