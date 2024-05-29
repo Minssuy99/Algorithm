cb05c70db669846b393cd115b1080501787078e6
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-29 19:02:12 +0900

    아스키 코드

---
+++ BOJ/Bronze/11654.cpp
@@ -0,0 +1,17 @@
+// 11654. 아스키 코드
+// https://www.acmicpc.net/problem/11654
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+
+    cin >> a;
+
+    cout << (int)a << endl;
+
+    return 0;
+}
