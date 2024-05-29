16a5f6f843072d0966b90ec61cfcb05fba632ab4
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-29 20:01:55 +0900

    문자와 문자열

---
+++ BOJ/Bronze/27866.cpp
@@ -0,0 +1,18 @@
+// 27866. 문자와 문자열
+// https://www.acmicpc.net/problem/27866
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string S;
+    int i;
+
+    cin >> S;
+    cin >> i;
+
+    cout << S[i - 1] << endl;
+
+    return 0;
+}
