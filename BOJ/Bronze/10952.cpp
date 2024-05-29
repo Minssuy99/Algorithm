1309dd0645a4d02f06b833c4c9fcb84a24869b61
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-29 11:02:43 +0900

    A + B -5

---
+++ BOJ/Bronze/10952.cpp
@@ -0,0 +1,30 @@
+// 10952. A + B - 5
+// https://www.acmicpc.net/problem/10952
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A , B = 0;
+
+        while (true)
+        {
+            cin >> A >> B;
+
+            if (0 < A && A < 10 && 0 < B && B < 10)
+            {
+
+                if (A == 0 && B == 0)
+                    break;
+
+                else
+                    cout << A + B << endl;
+            }
+            else
+                break;
+        }
+
+    return 0;
+}
