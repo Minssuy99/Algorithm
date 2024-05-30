eaa75b34d16835f5d0a7b012d10c415b2b6de1c4
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-30 22:27:49 +0900

    단어의 개수

---
+++ BOJ/Bronze/1152.cpp
@@ -0,0 +1,43 @@
+// 1152. 단어의 개수
+// https://www.acmicpc.net/problem/1152
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+
+    int word = 1;
+
+    getline(cin, s);
+
+    for(int i =0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+                word++;
+        }
+    }
+
+    if (s[0] == ' ')
+    {
+        word--;
+    }
+
+    if (s[s.length() -1] == ' ')
+    {
+        word--;
+    }
+
+    if (s[0] == ' ' && s.length() == 1)
+    {
+        word = 0;
+    }
+
+    cout << word << "\n";
+
+    return 0;
+}
