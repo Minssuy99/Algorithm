e9d8992ec98ab1b76916cc2d2e5dbf92a01b7250
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-30 21:24:25 +0900

    문자열 반복

---
+++ BOJ/Bronze/2675.cpp
@@ -0,0 +1,33 @@
+// 2675. 문자열 반복
+// https://www.acmicpc.net/problem/2675
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+
+    int repeat;
+
+    string s;
+
+    cin >> T;
+
+    for (int i = 0; i< T; i++)
+    {
+        cin >> repeat >> s;
+
+        for (int j = 0; j < s.length(); j++)
+        {
+            for (int k = 0; k < repeat; k++)
+            {
+                cout << s[j];
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
