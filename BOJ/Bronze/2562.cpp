d8a080e68c5c7bf9d26d02dc8014b42d333b6350
Minssuy99 <rlaalstjd6302@naver.com>
2024-05-30 10:13:12 +0900

    최댓값

---
+++ BOJ/Bronze/2562.cpp
@@ -0,0 +1,29 @@
+// 2562. 최댓값
+// https://www.acmicpc.net/problem/2562
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int highest = 0;
+    int highest_n = 0;
+
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> n;
+
+        if (n > highest)
+        {
+            highest = n;
+            highest_n = i + 1;
+        }
+    }
+
+    cout << highest << endl;
+    cout << highest_n << endl;
+
+    return 0;
+}
