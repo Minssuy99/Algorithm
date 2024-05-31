da2ca6d9842fe68db0ef65045242b882ca6bcf76
Minssuy99 <rlaalstjd6302@naver.com>
2024-06-01 04:13:57 +0900

    나머지

---
+++ BOJ/Bronze/3052.cpp
@@ -0,0 +1,33 @@
+// 3052. 나머지
+// https://www.acmicpc.net/problem/3052
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int arr[10] = {};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    int diff = 1;
+
+    for (int i = 0; i < arr_size; i++)
+    {
+        cin >> arr[i];
+        arr [i] %= 42;
+    }
+    sort(arr, arr + 10);
+    
+    for (int i = 0; i < arr_size-1; i++)
+    {
+        if(arr[i] != arr[i+1])
+        {
+            diff++;
+        }
+    }
+
+    cout << diff << endl;
+
+    return 0;
+}
