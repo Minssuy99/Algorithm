b19ea7fadde299742c94c0351c3788272f3c2089
Minssuy99 <rlaalstjd6302@naver.com>
2024-06-01 05:45:38 +0900

    ACM호텔

---
+++ BOJ/Bronze/tempCodeRunnerFile.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T, H, W, customer;
+
+    int index = 0;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> H >> W >> customer;
+
+        int *room = new int[W*H]; // 101 201 301 401 501 102 202 302..
+
+        for (int j = 1; j <= W; j++)
+        {
+            for (int k = 1; k <= H; k++)
+            {
+                room[index] = (k*100) + j;
+                index++;
+            }
+        }
+        cout << room[customer-1] << endl;
+    }
+
+    return 0;
+}
