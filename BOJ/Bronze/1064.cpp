ee0556c13c2a0ee3372ce84011a785783d3f7be5
Minssuy99 <rlaalstjd6302@naver.com>
2024-10-09 18:39:10 +0900

    평행사변형 (진행중)

---
+++ BOJ/Bronze/1064.cpp
@@ -0,0 +1,19 @@
+/// 1064. 평행사변형
+// https://www.acmicpc.net/problem/1064
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Ax,Ay,Bx,By,Cx,Cy;
+
+    double case_A, case_B, case_C;
+
+    cin >> Ax >> Ay >> Bx >> By >> Cx >> Cy;
+
+    case_A = 
+
+    return 0;
+}
