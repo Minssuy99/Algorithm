3ea3cc988062c45a3ff328a9e33ce0c29b6b79b9
Minssuy99 <rlaalstjd6302@naver.com>
2024-10-09 17:53:21 +0900

    스택 수열

---
+++ BOJ/Bronze/1874.cpp
@@ -0,0 +1,54 @@
+/// 1874. 스택 수열
+// https://www.acmicpc.net/problem/1874
+
+#include <iostream>
+#include <stack>
+
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int seq[1000001];
+    string result;
+
+    stack<int> st;
+
+    int n;
+    int num;
+    int index = 0;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> num;
+        seq[i] = num;
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        st.push(i);
+        result += "+\n";
+
+        while(!st.empty() && seq[index] == st.top())
+        {
+            st.pop();
+            result += "-\n";
+            index++;
+        }
+    }
+
+    if(st.empty())
+    {
+        cout << result;
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+
+}
