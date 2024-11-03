d4bd9e45c4262e9915af37eb1666936286221a4b
Minssuy99 <rlaalstjd6302@naver.com>
2024-11-03 21:52:12 +0900

    시험 점수

---
+++ BOJ/Bronze/5596.cpp
@@ -0,0 +1,36 @@
+// 5596. 시험 점수
+// https://www.acmicpc.net/problem/5596
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int s = 0;
+    int t = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int number;
+        cin >> number;
+        s += number;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        int number;
+        cin >> number;
+        t += number;
+    }
+
+    if (s >= t)
+        cout << s << '\n';
+    else
+        cout << t << '\n';
+
+    return 0;
+}
