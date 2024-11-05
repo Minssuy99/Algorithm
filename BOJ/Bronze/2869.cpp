d98f99596048cbf1c23381832f93642ce07e9eca
Minssuy99 <rlaalstjd6302@naver.com>
2024-11-05 17:10:21 +0900

    달팽이는 올라가고싶다

---
+++ BOJ/Bronze/2869.cpp
@@ -0,0 +1,31 @@
+// 2869. 달팽이는 올라가고싶다
+// https://www.acmicpc.net/problem/2869
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, v;
+
+    int result = 0;
+
+    cin >> a >> b >> v;
+
+    if ((v - a) % (a - b) == 0)
+    {
+        result = (v - a) / (a - b);
+    }
+    else
+    {
+        result = (v - a) / (a - b) + 1;
+    }
+
+    cout << result + 1 << '\n';
+
+    return 0;
+}
