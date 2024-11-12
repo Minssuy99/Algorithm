9b6789d2ca59f182ed797d41165a8a4ebd9f764c
Minssuy99 <rlaalstjd6302@naver.com>
2024-11-12 14:26:25 +0900

    요세푸스 문제0

--- BOJ/Silver/11866.cpp
+++ BOJ/Silver/11866.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     queue<int> q;
-    int n, k;   // 1 ~ n 번까지 사람이 있고, k번째 사람을 제거한다.
+    int n, k; // 1 ~ n 번까지 사람이 있고, k번째 사람을 제거한다.
 
     cin >> n >> k;
 
@@ -20,17 +20,17 @@ int main()
 
     cout << "<";
 
-    while(n)    // 사람 수
+    while (n) // 사람 수
     {
         for (int i = 1; i < k; i++)
         {
             q.push(q.front());
-            q.pop();   // 2 3 4 5 6 7 1 > 3 4 5 6 7 1 2
+            q.pop(); // 2 3 4 5 6 7 1 > 3 4 5 6 7 1 2
         }
 
         cout << q.front();
 
-        if (q.size() != 1)  // 마지막 콤마 생략
+        if (q.size() != 1) // 마지막 콤마 생략
         {
             cout << ", ";
         }
