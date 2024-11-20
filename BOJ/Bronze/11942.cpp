6b4f3ad075b7b37fe2c4f6f4cb6d0bd8e5aaa85d
Minssuy99 <rlaalstjd6302@naver.com>
2024-11-20 11:34:45 +0900

    고려대는 사랑입니다

---
+++ BOJ/Bronze/11942.cpp
@@ -0,0 +1,16 @@
+// 11942. 고려대는 사랑입니다
+// https://www.acmicpc.net/problem/11942
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "고려대학교";
+
+    return 0;
+}
