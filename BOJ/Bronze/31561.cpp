08f408c6ad0cb118a3519e753bd7fd63039ceaac
Minssuy99 <rlaalstjd6302@naver.com>
2024-11-20 23:48:13 +0900

    시계탑

---
+++ BOJ/Bronze/31561.cpp
@@ -0,0 +1,36 @@
+// 31561. 시계탑
+// https://www.acmicpc.net/problem/31561
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    double m;
+
+    cin >> m;
+
+    if (m <= 30)
+    {
+        cout << fixed;
+        cout.precision(1);
+        cout << m / 2 << '\n';
+        return 0;
+    }
+    else
+    {
+        m -= 30;
+        m = m * 3 / 2;
+
+        m += 15;
+        cout << fixed;
+        cout.precision(1);
+        cout << m << '\n';
+    }
+
+    return 0;
+}
