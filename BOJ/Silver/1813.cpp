8268c82849b899d98c5bf018fac76affb7d05330
Minssuy99 <rlaalstjd6302@naver.com>
2024-11-25 20:09:54 +0900

    논리학 교수

---
+++ BOJ/Silver/1813.cpp
@@ -0,0 +1,38 @@
+// 1813. 논리학 교수
+// https://www.acmicpc.net/problem/1813
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int arr[51] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        int number;
+        cin >> number;
+        arr[number]++;
+    }
+
+    int best = -1;
+
+    for (int i = 0; i < 51; i++)
+    {
+        if (arr[i] == i)
+        {
+            best = i;
+        }
+    }
+
+    cout << best;
+
+    return 0;
+}
