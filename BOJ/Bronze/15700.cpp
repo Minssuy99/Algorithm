e945ab27e2a5a00c12a9b617189f14c6832744a0
Minssuy99 <rlaalstjd6302@naver.com>
2024-12-03 12:26:23 +0900

    타일 채우기 4

---
+++ BOJ/Bronze/15700.cpp
@@ -0,0 +1,20 @@
+// 15700. 타일 채우기4
+// https://www.acmicpc.net/problem/150700
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    unsigned long long a, b;
+
+    cin >> a >> b;
+
+    cout << a * b / 2 << '\n';
+
+    return 0;
+}
